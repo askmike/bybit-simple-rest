02764a4e6e3a8203b0277a143336aaa08720d79b
Mike van Rossum <mike@mikevanrossum.nl>
2023-10-15 15:12:39 +0800

    add more example calls

--- sample.js
+++ sample.js
@@ -3,7 +3,6 @@ const BybitRest = require('./');
 const key = 'x';
 const secret = 'y';
 
-
 const run = async() => {
 
   const client = new BybitRest({
@@ -11,20 +10,47 @@ const run = async() => {
     secret
   });
 
-  try {
-    const resp = await client.request({
-      method: 'GET',
-      path: '/v5/account/wallet-balance',
-      data: {
-        accountType: 'CONTRACT'
-      }
-    });
+  /*
+  const resp = await client.request({
+    method: 'GET',
+    path: '/v5/account/wallet-balance',
+    data: {
+      // NOTE: provide in alphabetical order
+      accountType: 'CONTRACT'
+    }
+  });
+  console.log(JSON.stringify(resp, null, 2));
+  */
 
-    console.log(JSON.stringify(resp, null, 2));
-  } catch(e) {
-    console.log(e.message);
-  }
+  /*
+  const resp = await client.request({
+    method: 'POST',
+    path: '/v5/order/create',
+    data: {
+      // NOTE: provide in alphabetical order
+      category: 'linear',
+      orderType: 'Limit',
+      positionIdx: 0,
+      price: '20000',
+      qty: '0.01',
+      side: 'Buy',
+      symbol: 'BTCUSDT',
+      timeInForce: 'PostOnly',
+    }
+  });
+  console.log(JSON.stringify(resp, null, 2));
+  */
 
+  const resp = await client.request({
+    method: 'POST',
+    path: '/v5/order/cancel-all',
+    data: {
+      // NOTE: provide in alphabetical order
+      category: 'linear',
+      symbol: 'BTCUSDT',
+    }
+  });
+  console.log(JSON.stringify(resp, null, 2));
 
 }
 
